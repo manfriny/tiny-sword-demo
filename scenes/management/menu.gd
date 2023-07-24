cf5f6ea8a8a7ef94ec83ec4495c3db9227428ae9
Manfriny Pereira <manfriny@gmail.com>
2023-07-24 17:29:01 -0300

    Aula 46 finalizada - menu criado

---
+++ scenes/management/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready() -> void:
+	for button in get_tree().get_nodes_in_group("button"):
+		button.pressed.connect(on_button_pressed.bind(button.name))
+
+func on_button_pressed(button_name: String) -> void:
+	match button_name:
+		"NewGame":
+			get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+		"Quit":
+			get_tree().quit()
+		_:
+			pass
+			
